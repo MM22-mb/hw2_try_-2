046437ee2430fffadab636620cdc604a415f9fa5
Matt <matthew.martin@kellogg.northwestern.edu>
2026-02-09 21:46:43 +0000

    Created table data types

--- db/migrate/20260209213922_create_movies.rb
+++ db/migrate/20260209213922_create_movies.rb
@@ -1,6 +1,10 @@
 class CreateMovies < ActiveRecord::Migration[8.1]
   def change
     create_table :movies do |t|
+      t.string "title"
+      t.integer "year_released"
+      t.string "rated"
+      t.integer "studio_id"
       t.timestamps
     end
   end
--- db/migrate/20260209213946_create_agents.rb
+++ db/migrate/20260209213946_create_agents.rb
@@ -1,6 +1,7 @@
 class CreateAgents < ActiveRecord::Migration[8.1]
   def change
     create_table :agents do |t|
+      t.string "name"
       t.timestamps
     end
   end
